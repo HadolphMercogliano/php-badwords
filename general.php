fb87752b71aa690a9f8d1da095b197af5093d1b4
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-03-10 10:48:56 +0100

    add abbreviazione echo

--- general.php
+++ general.php
@@ -29,13 +29,13 @@ $censored_paragraph = str_replace($badword, "***" ,$init_paragraph );
     <div class="container">
       <h2 class="text-danger">Questo è il tuo paragrafo</h2>
       <p>
-        <?php echo $init_paragraph;?>    
+        <?= $init_paragraph;?>    
       </p>
-      <p>Il tuo paragrafo è lungo <b><?php echo strlen($init_paragraph); ?></b> caratteri</p>
+      <p>Il tuo paragrafo è lungo <b><?= strlen($init_paragraph); ?></b> caratteri</p>
   
       <h3 class="text-danger">Questo è il tuo paragrafo censurato</h3>
       <p>
-        <?php echo $censored_paragraph;?>
+        <?= $censored_paragraph;?>
       </p>
         <p>Il tuo paragrafo censurato è lungo <b><?php echo strlen($censored_paragraph); ?></b> caratteri </p>
     </div>
